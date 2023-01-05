716534f170515bfae36bdd3e13e4dd9a43b808ba
Dmytro Akulinin <akulinind@gmail.com>
2023-01-05 11:44:27 +0200

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Dmytro Akulinin on 05.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
